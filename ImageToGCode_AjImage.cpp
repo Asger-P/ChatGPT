0e9f1ea92d9d18ad4a1366f748700a333b572c10
Asger-P <45823737+Asger-P@users.noreply.github.com>
2025-09-02 23:32:38 +0200

    Upload ImageToGCode_AjImage.cpp

--- ImageToGCode_AjImage.cpp
+++ ImageToGCode_AjImage.cpp
@@ -5,4 +5,6 @@
 #include "AjImage.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+
+Now there must be tested to see if it actually  works.
 «
